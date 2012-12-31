019e94575d7ad99156dc2017b33c1f08da102447
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2012-12-30 17:03:34 -0800

    Remove naive break-early logic
    In single_source_shortest_paths(),` as soon as the destination node `d`
    was visited, the algorithm would break early for performance reasons,
    the logic being that there's no need to explore the rest of the graph if
    `d` has already been found.
    
    The problem with this approach is that it's possible to reach `d`
    without having actually found the shortest path to it (duh!).
    
    An approach that might work would be to record which nodes `d` has been
    visited from and break early if `d` has been visited from *all* of it's
    neighbors.
    
    refs #1

--- tests.js
+++ tests.js
@@ -25,13 +25,16 @@ assert.deepEqual(path, ['a', 'd', 'e', 'f', 'i']);
 
 
 graph = {
-  a: {b: 1, c: 100},
-  b: {c: 1},
-  d: {b: 1}
+  a: {b: 10, c: 100, d: 1},
+  b: {c: 10},
+  d: {b: 1, e: 1},
+  e: {f: 1},
+  f: {c: 1},
+  g: {b: 1}
 };
 // Reachable destination
 path = find_path(graph, 'a', 'c');
-assert.deepEqual(path, ['a', 'b', 'c']);
+assert.deepEqual(path, ['a', 'd', 'e', 'f', 'c']);
 path = find_path(graph, 'd', 'b');
 assert.deepEqual(path, ['d', 'b']);
 // Unreachable destination
@@ -39,7 +42,7 @@ assert.throws(function () {
   find_path(graph, 'c', 'a');
 }, Error);
 assert.throws(function () {
-  find_path(graph, 'a', 'd');
+  find_path(graph, 'a', 'g');
 }, Error);
 // Nonexistent destination
 assert.throws(function () {
@@ -47,4 +50,11 @@ assert.throws(function () {
 }, Error);
 // All paths from 'a'
 paths = dijkstra.single_source_shortest_paths(graph, 'a');
-assert.deepEqual(paths, {b: 'a', c: 'b'});
+assert.deepEqual(paths, {
+  d: 'a', 
+  b: 'd', 
+  c: 'b', 
+  e: 'd', 
+  f: 'e', 
+  c: 'f'
+});
